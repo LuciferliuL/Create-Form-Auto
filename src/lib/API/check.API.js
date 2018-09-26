d7e4a1f4194f87888a748281b3220eaf5a401696
luciferL <luciferlikeme@163.com>
2018-09-26 14:38:16 +0800

    API修改

--- src/lib/API/check.API.js
+++ src/lib/API/check.API.js
@@ -1,19 +1,21 @@
+const baseAPI = 'http://10.3.4.177:9004'
+
 export const API = (key) => {
     switch (key) {
         case 'CheckFormList':
             return {
                 method: 'GET',
-                http: 'http://10.2.132.127:7000/api/DataForm'
+                http: baseAPI + '/api/DataForm'
             }
         case 'SaveForm':
             return {
                 method: 'POST',
-                http: 'http://10.2.132.127:7000/api/DataForm'
+                http: baseAPI + '/api/DataForm'
             }
         case 'SQL':
             return {
                 method: 'POST',
-                http: 'http://10.2.132.127:7000/api/dataquery/GetDataBySqlName'
+                http: baseAPI + '/api/dataquery/GetDataBySqlName'
             }
         default:
             break;
