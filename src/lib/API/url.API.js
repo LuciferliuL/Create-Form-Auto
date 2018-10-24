cd5cfc864178a27a918e49d4e03c2f305507814d
wangq <wq@jzt.com>
2018-10-24 16:54:28 +0800

    api

--- src/lib/API/url.API.js
+++ src/lib/API/url.API.js
@@ -1,4 +1,11 @@
+//pro
+// global.cfg = {
+//     userAPI: 'http://10.3.4.233:20296',//sys
+//     baseAPI: 'http://10.3.4.172:9004'//query
+// }
+
+//test
 global.cfg = {
-    userAPI: 'http://10.3.4.233:20296',
-    baseAPI: 'http://10.3.4.177:9004'
+    userAPI: 'http://10.3.4.177:20296',//sys
+    baseAPI: 'http://10.3.4.177:9004'//query
 }
