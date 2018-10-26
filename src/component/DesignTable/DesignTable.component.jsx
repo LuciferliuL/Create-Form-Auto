4c59fec8721c600d55f0ba9897573c585e1d9374
wangq <wq@jzt.com>
2018-10-26 14:39:13 +0800

    error

--- src/component/DesignTable/DesignTable.component.jsx
+++ src/component/DesignTable/DesignTable.component.jsx
@@ -55,7 +55,7 @@ class DesignTablecomponent extends Component {
         }, {
             title: '最后修改时间',
             dataIndex: 'LastModifyTime',
-            width: '30%',
+            width: '15%',
             key: 'LastModifyTime',
             render: (text, record) => {
                 return (
