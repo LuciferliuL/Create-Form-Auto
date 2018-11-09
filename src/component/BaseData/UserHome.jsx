c60bfb4c6068b66ed73f72893836219a694cf9d4
gaoxueyi098 <ex-gaoxueyi600@pingan.com.cn>
2018-11-09 09:46:19 +0800

    添加菜单

--- src/component/BaseData/UserHome.jsx
+++ src/component/BaseData/UserHome.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import {Route} from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import { connect } from 'react-redux';
-import { Layout } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
 import UserList from './UserList'
 import UserInfo from './UserInfo'
 import UserAuthorization from './UserAuthorization'
@@ -17,16 +17,34 @@ function mapStateToProps(state) {
     };
 }
 
+
 class UserHome extends Component {
+    TitleClick = (e) => {
+        // this.setState({ current: e.key, });
+        this.props.history.push("/" + e.key);
+    }
     render() {
         return (
             <div>
-                <Layout>
-                    <Sider>
-
+                <Layout style={{ background: "#fff" }}>
+                    <Sider style={{ background: "#fff" }}>
+                        <Menu
+                            defaultSelectedKeys={['1']}
+                            defaultOpenKeys={['sub1']}
+                            mode="inline"
+                            theme="light"
+                            onClick={this.TitleClick}
+                        >
+                            <Menu.Item key="BaseData/UserList">
+                                人员信息维护
+                            </Menu.Item>
+                            <Menu.Item key="BaseData/DBList">
+                                数据库信息
+                            </Menu.Item>
+                        </Menu>
                     </Sider>
                     <Layout>
-                        <Header>Header</Header>
+                        {/* <Header style={{ background: "#fff" }}>Header</Header> */}
                         <Content>
                             <Route path="/BaseData/UserList" component={UserList}></Route>
                             <Route path="/BaseData/UserInfo" component={UserInfo}></Route>
@@ -34,7 +52,7 @@ class UserHome extends Component {
                             <Route path="/BaseData/DBList" component={DBList}></Route>
                             <Route path="/BaseData/DBInfo" component={DBInfo}></Route>
                         </Content>
-                        <Footer>Footer</Footer>
+                        {/* <Footer>Footer</Footer> */}
                     </Layout>
                 </Layout>
             </div>
