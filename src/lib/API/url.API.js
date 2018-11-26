8ebcbe361ba57ca0a3cae57672c0d1334429937b
wangq <wq@jzt.com>
2018-11-26 15:38:21 +0800

    i9

--- src/lib/API/url.API.js
+++ src/lib/API/url.API.js
@@ -22,7 +22,7 @@ global.msgcfg = {
     fileurl: "http://10.3.2.21:9003/"//excel保存站点
 }
 
-sessionStorage.setItem('environment', 'develop');//test,product,develop
+sessionStorage.setItem('environment', 'test');//test,product,develop
 const environment = sessionStorage.getItem('environment');
 if (environment === 'test') {
     global.login = {
@@ -42,8 +42,8 @@ else if (environment === 'product') {
         appSecret: "erp",//
         pub: "XT-abd10904-f8d6-47cc-8be1-32c1cb18a229",
         pubsercet: "11a8551ab4d10bf786faba3740934a89",
-        corntemplateid: "I9MessageSendhw",
-        autotemplateid: "I9MessageSendhw",
+        corntemplateid: "I9MessageSend",
+        autotemplateid: "I9MessageSend",
         filepath: "C:\\Program Files (x86)\\JZTERP\\CenterQuerybuild",//design路径
         fileurl: "http://10.3.4.233:9003/"//design站点
     }
