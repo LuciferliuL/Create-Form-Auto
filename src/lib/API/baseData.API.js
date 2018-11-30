93621b4862e75d5144670716bd46ee2de51291e9
wangq <wq@jzt.com>
2018-11-30 14:09:53 +0800

    api

--- src/lib/API/baseData.API.js
+++ src/lib/API/baseData.API.js
@@ -11,42 +11,42 @@ export const API = (key) => {
         // 用户列表查询
         case 'userList':
             return {
-                http: global.login.API + 'api/DataBaseOperation/GetDBAccount'
+                http: global.login.api + 'api/DataBaseOperation/GetDBAccount'
             }
         // 用户信息保存以及更新
         case 'saveUserInfo':
             return {
                 method: 'POST',
-                http: global.login.API + 'api/DataBaseOperation/SaveDBAccount'
+                http: global.login.api + 'api/DataBaseOperation/SaveDBAccount'
             }
         // 数据库列表查询
         case 'dbList':
             return {
                 method: "GET",
-                http: global.login.API + 'api/DataBaseOperation/GetDBInfo'
+                http: global.login.api + 'api/DataBaseOperation/GetDBInfo'
             }
         // 数据库信息保存以及更新
         case 'saveDbInfo':
             return {
                 method: 'POST',
-                http: global.login.API + 'api/DataBaseOperation/SaveDbInfo'
+                http: global.login.api + 'api/DataBaseOperation/SaveDbInfo'
             }
         // 获取用户授权信息
         case 'getAuthorList':
             return {
                 method: 'POST',
-                http: global.login.API + 'api/DataBaseOperation/GetDBPermissionsByAccount'
+                http: global.login.api + 'api/DataBaseOperation/GetDBPermissionsByAccount'
             }
         // 保存授权信息
         case 'saveAuthorInfo':
             return {
                 method: 'POST',
-                http: global.login.API + 'api/DataBaseOperation/SaveDBPermissions'
+                http: global.login.api + 'api/DataBaseOperation/SaveDBPermissions'
             }
         case 'getActionList':
             return {
                 method: 'POST',
-                http: global.login.API + 'api/DataBaseOperation/SaveDbInfo'
+                http: global.login.api + 'api/DataBaseOperation/SaveDbInfo'
             }
 
         default:
