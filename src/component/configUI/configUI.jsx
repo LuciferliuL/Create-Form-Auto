fdce8b24ee11748404a56e75993232db17aa8c8b
luciferL <luciferlikeme@163.com>
2018-12-25 15:36:06 +0800

    UI修改

--- src/component/configUI/configUI.jsx
+++ src/component/configUI/configUI.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Row, Col, Menu } from 'antd'
+import {  Menu, Button, Layout,Tag, Dropdown } from 'antd'
 import { API } from '../../lib/API/configui'
 import { POST$ } from '../../lib/MATH/math'
 import { menu } from './configui'
 
 const SubMenu = Menu.SubMenu;
+const { Header, Footer, Sider, Content, } = Layout
 function mapStateToProps(state) {
     return {
 
@@ -15,20 +16,22 @@ function mapStateToProps(state) {
 class configUI extends Component {
     state = {
         menuNode: [],
-        UIhref : "http://10.3.4.233:8090"
+        UIhref: "http://10.3.4.233:8090",
+        user: JSON.parse(sessionStorage.getItem('values')),
+        userdata: JSON.parse(sessionStorage.getItem('udata')),
     }
     componentDidMount() {
         POST$(API('GetConfigMenu').http, { "level": "14" }, (res) => {
-            console.log(menu(res));
+            // console.log(menu(res));
             this.setState({
                 menuNode: menu(res)
             })
         })
     }
     menuClick = (record, e) => {
-        console.log(record);
+        // console.log(record);
         this.setState({
-            UIhref:"http://10.3.4.233:8090" + record.Param2
+            UIhref: "http://10.3.4.233:8090" + record.Param2
         })
     }
     menu = (data, list = []) => {
@@ -46,9 +49,12 @@ class configUI extends Component {
         })
         return list
     }
+    enter = () => {
+        this.props.history.push('/loginLeader')
+    }
     render() {
         let h = document.documentElement.clientHeight
-        const { menuNode,UIhref } = this.state
+        const { menuNode, UIhref,  userdata } = this.state
         let m = []
         if (menuNode.length > 0) {
             menuNode.forEach(e => {
@@ -59,22 +65,41 @@ class configUI extends Component {
                 )
             })
         }
-
+        const menu = (
+            <Menu>
+                <Menu.Item>
+                    <a onClick={this.enter.bind(this)}>退出登录</a>
+                </Menu.Item>
+            </Menu>
+        );
         return (
-            <Row>
-                <Col span={3}>
-                    <Menu
-                        // defaultOpenKeys={['0submenu']}
-                        style={{height: h }}
-                        mode="inline"
-                        theme="dark">
-                        {m}
-                    </Menu>
-                </Col>
-                <Col span={21}>
-                    <iframe src={UIhref} frameBorder="0" style={{ width: '100%', height: h - 10 }}></iframe>
-                </Col>
-            </Row>
+            <Layout>
+                <Sider>  <Menu
+                    // defaultOpenKeys={['0submenu']}
+                    style={{ height: h }}
+                    mode="inline"
+                    theme="dark">
+                    {m}
+                </Menu></Sider>
+                <Layout>
+                    <Header>
+                        <div style={{ float: 'right' }}>
+                            <Tag color='blue' style={{ float: 'left', marginTop: '25px', background: 'none', border: 'none', fontSize: '14px', color: '#fff' }}>
+                                {userdata.Organization.OrganizationName}
+                            </Tag>
+                            <Dropdown overlay={menu} placement="bottomCenter">
+                                <Tag style={{ float: 'left', marginTop: '25px', background: 'none', border: 'none', fontSize: '14px', color: '#fff' }}>{userdata.UserName}</Tag>
+                            </Dropdown>
+                        </div>
+                    </Header>
+                    <Content>
+                        <iframe src={UIhref} frameBorder="0" style={{ width: '100%', height: h - 10 }}></iframe>
+                    </Content>
+                </Layout>
+            </Layout>
+
+
+
         );
     }
 }
